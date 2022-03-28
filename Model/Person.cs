58e817431792b3a5afa644e9db395afd95e1e41c
AllanKley <allanlkley@gmail.com>
2022-03-28 16:23:07 -0300

    vamos ver se dá ruim

---
+++ Model/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace Model;
+public class Person
+{
+    protected string name;
+    protected string age;
+    protected string document;
+    protected string email;
+    protected string phone;
+    protected string login;
+    protected Address address;
+}
