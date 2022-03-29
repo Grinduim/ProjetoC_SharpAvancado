3d65cf594c8822bed538ab09581d66f397cfd5ce
Grinduim <viniciusgribelperson@gmail.com>
2022-03-29 14:39:45 -0300

    teste

--- Model/Store.cs
+++ Model/Store.cs
@@ -5,5 +5,9 @@ public class Store
     private String CNPJ = "";
     private Owner owner;
     private List<Purchase> purchases = new List<Purchase>();
+
+    public void setName(string name){
+        this.name = name;
+    }
     
 }
