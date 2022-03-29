fc609b911ac8289b3e9c2989e8869654e1720929
AllanKley <allanlkley@gmail.com>
2022-03-29 16:17:36 -0300

    stores prontos

--- Model/Purchase.cs
+++ Model/Purchase.cs
@@ -6,58 +6,40 @@ public class Purchase
     private String number_confirmation = "";
     private String number_nf= "";
     private Client client;
+    private Store stores;
     private List<Product> products = new List<Product>();
-    private List<Store> stores = new List<Store>();
-
-    public DateTime getDatePurchase()
-    {
-        return this.date_purchase;
-    }
-    public void setDatePurchase(DateTime date_purchase)
-    {
-        this.date_purchase = date_purchase;
-    }
 
 
-    public String getPayment()
-    {
-        return this.payment;
-    }
-    public void setPayment(string payment)
-    {
-        this.payment = payment;
-    }
-
-
-    public String getNumberConfirmation()
-    {
-        return this.number_confirmation;
-    }
-    public void setNumberConfirmation(string number_confirmation)
-    {
-        this.number_confirmation = number_confirmation;
-    }
-
-
-    public String getNumberNf()
-    {
-        return this.number_nf;
-    }
-    public void setNumberNf(string number_nf)
-    {
-        this.number_nf = number_nf;
-    }
-
-
-    public Client GetClient()
-    {
-        return this.client;
-    }
-    public void setClient(Client client)
+    public Purchase(Client client, Store stores)
     {
         this.client = client;
+        this.stores = stores;
     }
-    //Allan, em vez de criar um get e set de client, cria um construtor de purchase que recebe esse objeto!!!!
-    //Assim voce lida com a dependencia e garante que purchase obrigatoriamente tem um client
+
+    public DateTime getDatePurchase(){return this.date_purchase;}
+    public void setDatePurchase(DateTime date_purchase){this.date_purchase = date_purchase;}
+
+
+    public String getPayment(){return this.payment;}
+    public void setPayment(string payment){this.payment = payment;}
+
+
+    public String getNumberConfirmation(){return this.number_confirmation;}
+    public void setNumberConfirmation(string number_confirmation){this.number_confirmation = number_confirmation;}
+
+
+    public String getNumberNf(){return this.number_nf;}
+    public void setNumberNf(string number_nf){this.number_nf = number_nf;}
+
+
+    public Client GetClient(){return this.client;}
+    public void setClient(Client client){this.client = client;}
+
+    public Store GetStores(){return this.stores;}
+    public void setStores(Store stores){this.stores = stores;}
+    
+
+    public List<Product> GetProducts(){return this.products;}
+    public void setProducts(List<Product> products){this.products = products;}
 }
 
