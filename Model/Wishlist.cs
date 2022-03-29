0e1e17dbd7f1c033015adb8afa12bd56a208cdd3
Grinduim <bizio.gabriel@gmail.com>
2022-03-29 14:24:50 -0300

    Adicionando method addproducttowishlist

--- Model/Wishlist.cs
+++ Model/Wishlist.cs
@@ -1,4 +1,11 @@
 namespace Model;
 public class Wishlist
 {
+    private List<Product> products = new List<Product>();
+
+    public List<Product> Products { get => products; set => products = value; }
+
+    public void addProductToWishList(Product product){
+        Products.Add(product);
+    }
 }
