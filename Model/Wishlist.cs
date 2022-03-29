e267aea9e6a31e910b07b8d00307651c328b6a13
Grinduim <bizio.gabriel@gmail.com>
2022-03-29 14:37:47 -0300

    commit: get e set wishlist + metodo de addproduct to wish list

--- Model/Wishlist.cs
+++ Model/Wishlist.cs
@@ -4,11 +4,22 @@ public class Wishlist
     private List<Product> products = new List<Product>();
     Client client;
     
+    public void setClient( Client client){
+        this.client = client;
+    }
 
-    public List<Product> Products { get => products; set => products = value; }
-    public Client Client { get => client; set => client = value; }
+    public Client getClient(Client client){
+        return this.client;
+    }
 
-    public void addProductToWishList(Product product){
-        Products.Add(product);
+    public List<Product> getProducts(){
+        return products;
+    }
+
+    public void addProductToWishList(Product product){ //add list to wishlist
+
+        if(!getProducts().Contains(product)){ // se a lista nao conter o produto na lusta
+            this.products.Add(product);// adicionar ela na lista
+        }
     }
 }
