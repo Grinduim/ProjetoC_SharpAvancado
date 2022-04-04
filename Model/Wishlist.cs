c6dda1a9139f503d53b280e9150c431880b95452
AllanKley <allanlkley@gmail.com>
2022-04-04 14:30:02 -0300

    tentando de novo

--- Model/Wishlist.cs
+++ Model/Wishlist.cs
@@ -1,14 +1,14 @@
 namespace Model;
-public class Wishlist
+public class WishList
 {
     private List<Product> products = new List<Product>();
     private Client client;
 
-    public Wishlist(Client client){this.client = client;}
+    public WishList(Client client){this.client = client;}
 
     public void setClient( Client client){this.client = client;}
     public Client getClient(Client client){return this.client;}
-
+    
     public List<Product> getProducts(){return products;}
     public void addProductToWishList(Product product){ 
         if(!getProducts().Contains(product)){ 
