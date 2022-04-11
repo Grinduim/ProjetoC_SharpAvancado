a5f8b01da8c1e950bdd952dc923b977f6d282b81
AllanKley <allanlkley@gmail.com>
2022-04-11 16:26:23 -0300

    a

--- console/Program.cs
+++ console/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using DAO;
+
+using(var context = new DaoContext()){
+    context.Database.EnsureCreated();
+
+    context.addresses.add(new Address{
+        street = "genoveva forlepa kopka 213",
+        city = "Pinhais",
+        state = "Paraná",
+        country = "Brasil",
+        poste_code = "83320560"  
+    });
+    
+    context.SaveChanges();
+}
+
+
+
