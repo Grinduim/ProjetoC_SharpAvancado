f5484016ad4d83724ea663bb4ba1e2ba87f536e1
AllanKley <allanlkley@gmail.com>
2022-04-18 13:26:35 -0300

    a

--- Model/Address.cs
+++ Model/Address.cs
@@ -3,11 +3,11 @@ using Interfaces;
 
 public class Address : IValidateDataObject<Address>
 {
-    private String street = "";
-    private String city = "";
-    private String state = "";
-    private String country = "";
-    private String poste_code = "";
+    private String street ;
+    private String city ;
+    private String state ;
+    private String country ;
+    private String poste_code ;
 
     public Address() { }
 
@@ -21,6 +21,8 @@ public class Address : IValidateDataObject<Address>
     }
 
 
+
+    #region GETS E SETS
     public void setStreet(String street) { this.street = street; }
     public String getStreet() { return this.street; }
 
@@ -39,7 +41,10 @@ public class Address : IValidateDataObject<Address>
 
     public void setPostalCode(String poste_code) { this.poste_code = poste_code; }
     public String getPostalCode() { return this.poste_code; }
+#endregion
+   
 
+   
     public Boolean validateObject(Address obj)
     {
 
@@ -50,5 +55,171 @@ public class Address : IValidateDataObject<Address>
         if (obj.getPostalCode() == null) return false;
         return true;
     }
+
+
+   
 }
 
+
+
+
+
+using System;
+using Interfaces; 
+using DAO;
+using DTO;
+using System.Collections.Generic;
+
+namespace Model;
+
+public class Address : IValidateDataObject, IDataController<AddressDTO, Address>
+{
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String country;
+
+    private String postal_code;
+
+    public List<AddressDTO> addressDTO = new List<AddressDTO>();
+
+    public Address(String street,String city, String state,String country , String postal_code){
+        this.street =  street;
+
+        this.city = city;
+
+        this.state = state;
+
+        this.country = country;
+
+        this.postal_code = postal_code;
+    }
+
+    public static Address convertDTOToModel(AddressDTO obj)
+    {
+        return new Address(obj.street, obj.city, obj.state, obj.country, obj.postal_code);
+    }
+
+
+    public Boolean validateObject()
+    {
+        return true;
+    }
+
+    public void delete(AddressDTO obj)
+    {
+
+    }
+
+    public int save()
+    {
+        var id = 0;
+
+        using(var context = new DAOContext())
+        {
+            var address = new DAO.Address{
+                street = this.street,
+                city = this.city,
+                state = this.state,
+                country = this.country,
+                postal_code = this.postal_code
+            };
+
+            context.Address.Add(address);
+
+            context.SaveChanges();
+
+            id = address.id;
+
+        }
+         return id;
+    }
+
+    public void update(AddressDTO obj)
+    {
+
+    }
+
+    public AddressDTO findById(int id)
+    {
+
+        return new AddressDTO();
+    }
+
+    public List<AddressDTO> getAll()
+    {        
+        return this.addressDTO;      
+    }
+
+   
+    public AddressDTO convertModelToDTO()
+    {
+        var addressDTO = new AddressDTO();
+
+        addressDTO.street = this.street;
+
+        addressDTO.state = this.state;
+
+        addressDTO.city = this.city;
+
+        addressDTO.country = this.country;
+
+        addressDTO.postal_code = this.postal_code;
+
+        return addressDTO;
+    }
+
+    public void setStreet(String street)
+    {
+        this.street = street;
+    }
+
+    public void setCity(String city)
+    {
+        this.city = city;
+    }
+
+    public void setState(String state)
+    {
+        this.state = state;
+    }
+
+    public void setCountry(String country)
+    {
+        this.country = country;
+    }
+
+    public void setPostalCode(String postal_code)
+    {
+        this.postal_code = postal_code;
+    }
+
+    public String getStreet()
+    {
+        return this.street;
+    }
+
+    public String getCity()
+    {        
+        return this.city;
+    }
+
+    public String getState()
+    {
+        return this.state;
+    }
+
+    public String getCountry()
+    {
+        return this.country;
+    }
+
+    public String getPostalCode()
+    {
+        return this.postal_code;
+    }
+
+}
