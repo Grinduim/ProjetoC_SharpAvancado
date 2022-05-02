fef72ec5483efa543ed3c5ad543baad577711b92
Grinduim <viniciusgribelperson@gmail.com>
2022-05-02 16:34:24 -0300

    arrumando conflitos

--- Controller/Controllers/AddressController.cs
+++ Controller/Controllers/AddressController.cs
@@ -9,54 +9,55 @@ namespace Controller.Controllers;
 public class AddressController
 {
 
-<<<<<<< HEAD
-        [HttpPost]
-        [Route("register")]
-        public object registerAddress([FromBody] AddressDTO address)
+    [HttpPost]
+    [Route("register")]
+    public object registerAddress([FromBody] AddressDTO address)
+    {
+        var addressModel = Model.Address.convertDTOToModel(address);
+        int id = addressModel.save();
+        return new
         {
-            var addressModel = Model.Address.convertDTOToModel(address);
-            int id = addressModel.save();
-            return new
-            {
-                response = "salvou on banco"
-            };
-        }
+            response = "salvou on banco"
+        };
+    }
 
-        [HttpDelete]
-        [Route("Delete")]
-        public void removeAddress([FromBody] AddressDTO address)
+    [HttpDelete]
+    [Route("Delete")]
+    public void removeAddress([FromBody] AddressDTO address)
+    {
+        var addressModel = Model.Address.convertDTOToModel(address);
+        addressModel.delete();
+        return new
         {
-            var addressModel = Model.Address.convertDTOToModel(address);
-            addressModel.delete();
-            return new {
-                status = "ok",
-                mensagem = "Excluido com sucesso"
-            };
+            status = "ok",
+            mensagem = "Excluido com sucesso"
+        };
 
-            
 
-        }
 
-        public void updateAddress(AddressDTO address)
+    }
+
+    public void updateAddress(AddressDTO address)
+    {
+    }
+
+    [HttpPost]
+    [Route("register")]
+    public object registerAddress([FromBody] AddressDTO address)
+    {
+        var addressModel = Model.Address.convertDTOToModel(address);
+        int id = addressModel.save();
+        return new
         {
-        }
+            response = "salvou on banco"
+        };
+    }
+
+    public void removeAddress(AddressDTO address)
+    {
+    }
+
+    public void updateAddress(AddressDTO address)
+    {
     }
 }
-=======
-[HttpPost]
-[Route("register")]
-public object registerAddress([FromBody]AddressDTO address){
-    var addressModel = Model.Address.convertDTOToModel(address);
-    int id = addressModel.save();
-    return new {
-        response = "salvou on banco"
-    };
-}   
-
-public void removeAddress(AddressDTO address){
-}
-
-public void updateAddress(AddressDTO address){
-}
-}
->>>>>>> 5156b67b7d0b0b21ca26158ca55d480a163f55f6
