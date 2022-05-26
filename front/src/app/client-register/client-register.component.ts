17155e6b1e2574ba97fcf71ef460da3faf7345c5
Grinduim <viniciusgribelperson@gmail.com>
2022-05-26 16:53:17 -0300

    validação forms name

--- front/src/app/client-register/client-register.component.ts
+++ front/src/app/client-register/client-register.component.ts
@@ -8,60 +8,34 @@ import { Client } from '../client';
   styleUrls: ['./client-register.component.css'],
 })
 export class ClientRegisterComponent implements OnInit {
-  clientForms: FormGroup;
-
-
-  constructor(private formBuilder: FormBuilder) {
-    this.clientForms = this.formBuilder.group({
-      'name': ['', [Validators.required, Validators.minLength(3)]],
-      'phone': ['', [Validators.required]],
-      'email': ['', [Validators.required, Validators.email]],
-      'document':['',[Validators.required]],
-      'birth':['',[Validators.required]],
-      'login':['',[Validators.required]],
-      'password':['',[Validators.required]]
-    });
-  }
+  // constructor(private formBuilder: FormBuilder) {
+  //   this.clientForms = this.formBuilder.group({
+  //     'name': ['', [Validators.required, Validators.minLength(3)]],
+  //     'phone': ['', [Validators.required]],
+  //     'email': ['', [Validators.required, Validators.email]],
+  //     'document':['',[Validators.required]],
+  //     'birth':['',[Validators.required]],
+  //     'login':['',[Validators.required]],
+  //     'password':['',[Validators.required]]
+  //   });
+  // }
 
   ngOnInit(): void {
+
   }
 
   VerifyForm() {
-    // let allInputs: Array<any> = [];
-    // let name = document.getElementById('name') as HTMLInputElement;
-    // this.clientForms.name = name.value;
-    // let phone = document.getElementById('phone') as HTMLInputElement;
-    // allInputs.push(phone);
-    // let email = document.getElementById('email') as HTMLInputElement;
-    // allInputs.push(email);
-    // let login = document.getElementById('login') as HTMLInputElement;
-    // allInputs.push(login);
-    // let birth = document.getElementById('birth') as HTMLInputElement;
-    // allInputs.push(birth);
-    // let pass = document.getElementById('password') as HTMLInputElement;
-    // allInputs.push(pass);
-    // let doc = document.getElementById('document') as HTMLInputElement;
-    // allInputs.push(doc);
-
-    // allInputs.forEach(function (prop) {
-    //   if (prop.value) {
-    //     console.log(prop);
-    //     console.log('aa');
-    //   }
-    // });
-    // if(name !=null){
-    //   console.log(name)
-    // }
-    // var data = JSON.stringify({
-    //   "name" : name.value,
-    // }
-
-    console.log("Chamou");
-    if(!this.clientForms.valid){
-      console.log("Errouu")
-
+    console.log("Loo")
+    var nameInput = document.querySelector("#name");
+    if(nameInput?.textContent == ""){
+      this.activeVisibleSpan("#name-none")
     }
-    console.log(this.clientForms.value)
+  }
+
+  activeVisibleSpan(id: string){
+    var span = document.querySelector(id);
+    span?.classList.remove('invisible');
+    console.log("aaa")
   }
 
 
